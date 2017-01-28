1b949fffe29dd557190cbf8173016c99ce3234f0
Robert StDenis <rstdenis@pc22.cs.ucdavis.edu>
2017-01-27 18:36:18 -0800

    Updated testing script

--- ExampleFiles/sshell_test.sh
+++ ExampleFiles/sshell_test.sh
@@ -17,204 +17,300 @@ path=$(pwd)
 correct=0
 total=12
 
+# binaries
+RM="rm -f"	# don't fail if file doesn't exist
+
 # Test correct exiting
 exit_test(){
   #echo -e "exit\n" | ../sshell 1> ../out_test 2> ../err_test
   echo -e "exit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
 
   test_str=$(sed '1q;d' $ERRFILE)
-  if [ $test_str == "Bye..." ]; then
+  corr_str="Bye..."
+
+  echo -n "exit_test -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
     let "correct++"
-    echo -e "exit test -- PASS\n"
+    echo "PASS"
   else
-    echo -e "exit test -- FAIL\n"
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
   fi
+  echo
 
   # clean up
-  rm $OUTFILE
-  rm $ERRFILE
+  $RM $OUTFILE
+  $RM $ERRFILE
 }
 
 # Tests for correct mkdir output
 mkdir_test(){
-  echo -e "mkdir test_d\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
+  test_dir="test_d"
+
+  echo -e "mkdir -p $test_dir\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
+
   test_str=$(sed '1q;d' $ERRFILE)
-  if [ "$test_str" == "+ completed 'mkdir test_d' [0]" ]; then
+  corr_str="+ completed 'mkdir -p $test_dir' [0]"
+
+  echo -n "mkdir test -- output only, not functionality -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
     let "correct"++
-    echo -e "mkdir test -- output only, not functionality -- PASS\n"
+    echo "PASS"
   else
-    echo -e "mkdir test -- output only, not functionality -- FAIL\n"
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
   fi
+  echo
 
   # clean up
-  rm $OUTFILE
-  rm $ERRFILE
-  rm -rf test_d
+  $RM $OUTFILE
+  $RM $ERRFILE
+  $RM -r "$test_dir"
 }
 
 # cd and pwd test
 cd_and_pwd_test(){
-  echo -e "mkdir test_d\ncd test_d\npwd\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '1q;d' $ERRFILE) 
+  test_dir="test_d"
+
+  echo -e "mkdir -p $test_dir\ncd $test_dir\npwd\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
+
+  test_str=$(sed '1q;d' $ERRFILE)
+  corr_str="+ completed 'mkdir -p $test_dir' [0]"
   test_str2=$(sed '4q;d' $OUTFILE)
+  corr_str2="$path/$TDIR/$test_dir"
 
-  if [ "$test_str" == "+ completed 'mkdir test_d' [0]" ] &&
-     [ "$test_str2" == "$path/$TDIR/test_d" ]; then
+  echo -n "cd and pwd -- "
+
+  if [ "$test_str" == "$corr_str" ] &&
+     [ "$test_str2" == "$corr_str2" ]; then
      let "correct"++
-     echo -e "cd and pwd -- PASS\n"
+     echo "PASS"
   else
-     echo -e "cd and pwd -- FAIL\n"
+     echo "FAIL\n"
+    echo "Got '$test_str' but expected '$corr_str'"
+    echo "Got '$test_str2' but expected '$corr_str2'"
   fi
-
-  # for checking if pwd was a syscall, outfile should be empty if syscall
-  strace -f sh -c 'echo -e "pwd\nexit\n" | ../sshell' | grep "usr/bin/pwd" > $OUTFILE
-  test_str=$(sed '1q;d' $OUTFILE)
-  if [ "$test_str" == "" ]; then
-     let "correct"++
-     echo -e "pwd is syscall check -- PASS\n"
-  else
-     echo -e "pwd is syscall check -- FAIL\n"
-  fi
-
+  echo
 
   cd $path/$TDIR
-  rm -rf test_d
-  rm $OUTFILE
-  rm $ERRFILE
+  $RM -r "$test_dir"
+  $RM $OUTFILE
+  $RM $ERRFILE
+
+}
+
+pwd_syscall_test() {
+  # for checking if pwd was a syscall, outfile should be empty if syscall
+  strace -f sh -c 'echo -e "pwd\nexit\n" | ../sshell' |& grep "usr/bin/pwd" > $OUTFILE
+
+  test_str=$(sed '1q;d' $OUTFILE)
+  corr_str=""
+
+  echo -n "pwd is syscall check -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
+     let "correct"++
+     echo "PASS"
+  else
+     echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+  fi
+  echo
+
+  $RM $OUTFILE
+  $RM $ERRFILE
 
 }
 
 # > redirect test -- requires cat and echo
 redirect_out_test(){
   echo -e "echo hello > t\ncat t\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
+
   test_str=$(sed '3q;d' $OUTFILE)
+  corr_str="hello"
 
-  if [ "$test_str" == "hello" ]; then
+  echo -n "> redirect test -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
     let "correct"++
-    echo -e "> redirect test -- PASS\n"
+    echo "PASS"
   else
-    echo -e "> redirect test -- FAIL\n"
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
   fi
+  echo
 
-  rm $OUTFILE
-  rm $ERRFILE
-  rm t
+  $RM $OUTFILE
+  $RM $ERRFILE
+  $RM t
 }
 
 # < redirect test -- requires grep and >
 redirect_in_test(){
   echo -e "echo hello > t\ngrep he < t\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '3q;d' $OUTFILE)
 
-  if [ "$test_str" == "hello" ]; then
+  test_str=$(sed '3q;d' $OUTFILE)
+  corr_str="hello"
+
+  echo -n "< redirect test -- requires > to work -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
     let "correct"++
-    echo -e "< redirect test -- requires > to work -- PASS\n"
+    echo "PASS"
   else
-    echo -e "< redirect test -- requires > to work -- FAIL\n"
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
   fi
-  
-  rm $OUTFILE
-  rm $ERRFILE
-  rm t
+  echo
+
+  $RM $OUTFILE
+  $RM $ERRFILE
+  $RM t
 }
 
 # pipe test
 pipe_test(){
   echo -e "echo hello world hello world | grep hello | wc -m\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '2q;d' $OUTFILE)
-  if [ "$test_str" == "24" ]; then
-    let "correct"++
-    echo -e "pipe test -- PASS\n"
-  else
-    echo -e "pipe test -- FAIL\n"
-  fi
 
-  rm $OUTFILE
-  rm $ERRFILE
+  test_str=$(sed '2q;d' $OUTFILE)
+  corr_str="24"
+
+  echo -n "pipe test -- "
+  if [ "$test_str" == "$corr_str" ]; then
+    let "correct"++
+    echo "PASS"
+  else
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+  fi
+  echo
+
+  $RM $OUTFILE
+  $RM $ERRFILE
 }
 
 # background & test
 background_test(){
   echo -e "sleep 1&\nsleep 2\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '2q;d' $OUTFILE)
-  test_str2=$(sed '1q;d' $ERRFILE)
-  test_str3=$(sed '2q;d' $ERRFILE)
-  if [ "$test_str" == "sshell$ sleep 2" ] &&
-     [ "$test_str2" == "+ completed 'sleep 1&' [0]" ] &&
-     [ "$test_str3" == "+ completed 'sleep 2' [0]" ]; then
-     let "correct"++
-     echo -e "Background test -- PASS\n"
-  else
-     echo -e "Background test -- FAIL\n"
-  fi
 
-  rm $OUTFILE
-  rm $ERRFILE
+  test_str=$(sed '2q;d' $OUTFILE)
+  corr_str="sshell$ sleep 2"
+  test_str2=$(sed '1q;d' $ERRFILE)
+  corr_str2="+ completed 'sleep 1&' [0]"
+  test_str3=$(sed '2q;d' $ERRFILE)
+  corr_str3="+ completed 'sleep 2' [0]"
+
+  echo -n "Background test -- "
+  if [ "$test_str" == "$corr_str" ] &&
+     [ "$test_str2" == "$corr_str2" ] &&
+     [ "$test_str3" == "$corr_str3" ]; then
+     let "correct"++
+     echo "PASS"
+  else
+     echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+    echo "Got '$test_str2' but expected '$corr_str2'"
+    echo "Got '$test_str3' but expected '$corr_str3'"
+  fi
+  echo
+
+  $RM $OUTFILE
+  $RM $ERRFILE
 
 }
 
 # check for correct output on invalid command
 invalid_cmd_test(){
   echo -e "d\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '1q;d' $ERRFILE)
-  test_str2=$(sed '2q;d' $ERRFILE)
-  if [ "$test_str" == "execvp: No such file or directory" ] &&
-     [ "$test_str2" == "+ completed 'd' [1]" ]; then
-     let "correct"++
-     echo -e "Invalid cmd test -- PASS\n"
-  else
-     echo -e "Invalid cmd test -- FAIL\n"
-  fi
 
-  rm $OUTFILE
-  rm $ERRFILE
+  test_str=$(sed '1q;d' $ERRFILE)
+  corr_str="execvp: No such file or directory"
+  test_str2=$(sed '2q;d' $ERRFILE)
+  corr_str2="+ completed 'd' [1]"
+
+  echo -n "Invalid cmd test -- "
+  if [ "$test_str" == "$corr_str" ] &&
+     [ "$test_str2" == "$corr_str2" ]; then
+     let "correct"++
+     echo "PASS"
+  else
+     echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+    echo "Got '$test_str2' but expected '$corr_str2'"
+  fi
+  echo
+
+  $RM $OUTFILE
+  $RM $ERRFILE
 }
 
 # < redirect error test
 invalid_in_test(){
   echo -e "echo h > t\ncat t | grep h < t\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '2q;d' $ERRFILE)
-  if [ "$test_str" == "Error: mislocated input redirection" ]; then
-    let "correct"++
-    echo -e "< redirect error test -- PASS\n"
-  else
-    echo -e "< redirect error test -- FAIL\n"
-  fi
 
-  rm t
-  rm $OUTFILE
-  rm $ERRFILE
+  test_str=$(sed '2q;d' $ERRFILE)
+  corr_str="Error: mislocated input redirection"
+
+  echo -n "< redirect error test -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
+    let "correct"++
+    echo "PASS"
+  else
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+  fi
+  echo
+
+  $RM t
+  $RM $OUTFILE
+  $RM $ERRFILE
 }
 
 # > redirect error test
 invalid_out_test(){
   echo -e "echo h > t | cat t\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '1q;d' $ERRFILE)
-  if [ "$test_str" == "Error: mislocated output redirection" ]; then
-    let "correct"++
-    echo -e "> redirect error test -- PASS\n"
-  else
-    echo -e "> redirect error test -- FAIL\n"
-  fi
 
-  rm t
-  rm $OUTFILE
-  rm $ERRFILE
+  test_str=$(sed '1q;d' $ERRFILE)
+  corr_str="Error: mislocated output redirection"
+
+  echo -n "> redirect error test -- "
+
+  if [ "$test_str" == "$corr_str" ]; then
+    let "correct"++
+    echo "PASS"
+  else
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+  fi
+  echo
+
+  $RM t
+  $RM $OUTFILE
+  $RM $ERRFILE
 }
 
 # & error test
 invalid_background_test(){
   echo -e "echo hello & | grep hello\nexit\n" | ../sshell 1> $OUTFILE 2> $ERRFILE
-  test_str=$(sed '1q;d' $ERRFILE)
-  if [ "$test_str" == "Error: mislocated background sign" ]; then
-    let "correct"++
-    echo -e "& error test -- PASS\n"
-  else
-    echo -e "& error test -- FAIL\n"
-  fi
 
-  rm $OUTFILE
-  rm $ERRFILE
+  test_str=$(sed '1q;d' $ERRFILE)
+  corr_str="Error: mislocated background sign"
+
+  echo -n "& error test -- "
+  if [ "$test_str" == "$corr_str" ]; then
+    let "correct"++
+    echo "PASS"
+  else
+    echo "FAIL"
+    echo "Got '$test_str' but expected '$corr_str'"
+  fi
+  echo
+
+  $RM $OUTFILE
+  $RM $ERRFILE
 }
 
 
@@ -225,6 +321,7 @@ run_all_tests(){
   exit_test
   mkdir_test
   cd_and_pwd_test
+  pwd_syscall_test
   redirect_out_test
   redirect_in_test
   pipe_test
@@ -236,10 +333,10 @@ run_all_tests(){
 }
 
 main_func(){
-  rm -rf $TDIR
+  $RM -r $TDIR
 
   # setting up tests
-  mkdir $TDIR
+  mkdir -p $TDIR
   cd $TDIR
 
   # run tests
@@ -249,7 +346,7 @@ main_func(){
 
   # clean up 
   cd ..
-  rm -rf $TDIR
+  $RM -r $TDIR
 }
 
 main_func
